fa56e0e764e2eea4ae0aba33f37c56ac6523bcd1
Ishmeet Singh <ishmeet.singh@magicedtech.com>
2024-09-06 16:40:17 +0530

    Task App

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const taskRoutes = require('./routes/tasks')
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
